dbd26618942454c0c69266046b617941b6c316b4
Dmitriy <krasav4erzt@gmail.com>
2017-11-29 15:11:04 +0200

    Dashboard

--- app/components/Home/DarinkaP/Projects/index.js
+++
@@ -1,2 +0,0 @@
-export { default as DarinkaProject1 } from './DarinkaProject1/DarinkaProject1.jsx'
-export { default as DarinkaProject2 } from './DarinkaProject2/DarinkaProject2.jsx'
--- app/components/Home/Krasav4er/Projects/index.js
+++
@@ -1,2 +0,0 @@
-export { default as Project1 } from './Project1/Project1.jsx'
-export { default as Project2 } from './Project2/Project2.jsx'
