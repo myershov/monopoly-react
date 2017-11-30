a2a3b0390902c4c3c4cf16e36a5f29428a13811e
Sergey Tkachuk <serhiytkachuk88@gmail.com>
2017-11-30 16:11:34 +0200

    fixed Login v.1.0

--- app/components/Login/Login.jsx
+++ app/components/Login/Login.jsx
@@ -6,19 +6,20 @@ class Login extends Component {
     return (
       <div id='login' className='form__login'>
         <form action='#' method='post'>
+          <img src='app/components/Type/Images/monopoly-brand.png' className='extended__logo' alt='extended logo' />
           <div className='form__wrap'>
-            <img src='#' alt='logo monopoly' />
+            <img src='app/components/Login/Images/logo.png' className='logo' alt='logo monopoly' />
             <div className='form__body'>
-              <h3 className='form__text'>Profile registration</h3>
-              <input type='text' name='username' placeholder='Enter your login' />
-              <input type='password' name='password' placeholder='Enter your password' />
+              <h2 className='form__text'>Реєстрація Профілю</h2>
+              <input type='text' name='username' placeholder="Введіть ваше ім'я" />
+              <input type='text' name='login' placeholder="Введіть ім'я гравця (Нікнейм)" />
+              <input type='password' name='password' placeholder='Введіть адресу електронної пошти' />
               <fieldset>
+                <legend>або увійдітьза допомогою:</legend>
                 <button type='button' className='play-button'>Facebook</button>
                 <button type='button' className='play-button'>Twitter</button>
               </fieldset>
-              <div className='sub__btn'>
-                <button type='submit' className='submit-button'>Join</button>
-              </div>
+              <button type='submit' className='submit-button'>Долучитися</button>
             </div>
           </div>
         </form>
