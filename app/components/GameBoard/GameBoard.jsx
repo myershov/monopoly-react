a0bc9f4e6a99060c343b87e172f354eeea5bedf6
Dmitriy <krasav4erzt@gmail.com>
2017-12-01 12:35:52 +0200

    added gameboard

--- app/components/GameBoard/GameBoard.jsx
+++ app/components/GameBoard/GameBoard.jsx
@@ -4,9 +4,13 @@ import './GameBoard.styl'
 
 class GameBoard extends Component {
   render () {
+    const test = [{orientation: '', text: 'START', id: 0}, {orientation: 'gorizontal', text: 'first cell mueseum', id: 1}]
     return (
-      <div className='monopoly-app'>
-        <GameBoardCell />
+      <div className='gameboard'>
+        <div className='top'>
+          {test.map(i => <GameBoardCell {...i} />)}
+          {test.map(i => <GameBoardCell {...i} />)}
+        </div>
       </div>
     )
   }
