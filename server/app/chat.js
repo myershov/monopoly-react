d8dedfaeaf415382e3151c92018fdb8b2101f7f0
Mykola Yershov <my.yershov@gmail.com>
2017-12-08 01:27:44 +0300

    Updated server main file

--- server/app/chat.js
+++ server/app/chat.js
@@ -1,11 +1,11 @@
-var exports = module.exports = {}
-
-exports.init = function (socket, io) {
+const chat = (socket, io) => {
   socket.broadcast.emit('user connected')
-  socket.on('chat message', function (msg, user) {
+  socket.on('chat message', (msg, user) => {
     io.emit('chat message', msg, user)
   })
-  socket.on('disconnect', function () {
+  socket.on('disconnect', () => {
     io.emit('user disconnect')
   })
 }
+
+module.exports = chat
--- server/index.js
+++ server/index.js
@@ -1,25 +1,29 @@
+import gameboard from './app/gameboard/gameboard.js'
+import autorization from './app/autorization.js'
+import socketio from 'socket.io'
+import chat from './app/chat.js'
 import express from 'express'
+import http from 'http'
+
 const app = express()
-const http = require('http').Server(app)
-const io = require('socket.io')(http)
+const server = http.Server(app)
+const io = socketio(server)
 const port = 8080
 
-require('./app/autorization.js').init(app, io)
-
+autorization.init(app, io)
 app.use(express.static(__dirname))
-
-let gameInfo = {
-  fieldsLen: '',
-  usersLen: '',
-  usersList: [],
-  listOfGames: []
-}
+// let gameInfo = {
+//   fieldsLen: '',
+//   usersLen: '',
+//   usersList: [],
+//   listOfGames: []
+// }
 io.on('connection', socket => {
-  require('./app/chat.js').init(socket, io)
-  require('./app/game.js').init(socket, io, gameInfo)
-  require('./app/createGame.js').init(socket, io, gameInfo)
-  require('./app/getGameList.js').init(socket, io, gameInfo)
+  chat(socket, io)
+  // require('./app/game.js').init(socket, io, gameInfo)
+  // require('./app/createGame.js').init(socket, io, gameInfo)
+  // require('./app/getGameList.js').init(socket, io, gameInfo)
 })
-require('./app/gameboard/gameboard.js').init(app)
+gameboard.init(app)
 console.log('Running')
-http.listen(port)
+server.listen(port)
