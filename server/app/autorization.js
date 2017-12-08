fec0d01b80664296ab80e2e172c7c6e3e47de484
Mykola Yershov <my.yershov@gmail.com>
2017-12-09 02:06:47 +0300

    Updated code

--- server/app/autorization.js
+++ server/app/autorization.js
@@ -1,42 +1,8 @@
-var passport = require('passport')
-var Strategy = require('passport-local').Strategy
-var db = require('./db')
+// var passport = require('passport')
+// var Strategy = require('passport-local').Strategy
+// var db = require('./db')
 
 module.exports = app => {
-  passport.use(new Strategy(
-    function (username, password, cb) {
-      db.users.findByUsername(username, function (err, user) {
-        if (err) { return cb(err) }
-        if (!user) { return cb(null, false) }
-        if (user.password !== password) { return cb(null, false) }
-        return cb(null, user)
-      })
-    }
-  ))
-  //
-  passport.serializeUser(function (user, cb) {
-    cb(null, user.id)
-  })
-
-  passport.deserializeUser(function (id, cb) {
-    db.users.findById(id, function (err, user) {
-      if (err) { return cb(err) }
-      cb(null, user)
-    })
-  })
-  //
-  // app.use(require('morgan')('combined'))
-  app.use(require('cookie-parser')())
-  // app.use(require('body-parser').urlencoded({ extended: true }))
-  // app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }))
-  app.use(passport.initialize())
-  // app.use(passport.session())
-  //
-  // app.post('/login', passport.authenticate('local', { failureRedirect: '/login' }), function (req, res) {
-  //   io.emit('save static user information', req.user)
-  //   res.redirect('/#')
-  // })
-  //  passport.authenticate('local', { successRedirect: '/', failureRedirect: '/2login', failureFlash: true })
   app.post('/login', (req, res) => {
     res.send({})
   })
