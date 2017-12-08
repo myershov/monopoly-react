7ac80e8b4c70bb69c867ff31ac792362edbd8260
Sergey Tkachuk <serhiytkachuk88@gmail.com>
2017-12-08 23:36:13 +0200

    stylization of gameboard

--- app/components/GameBoard/components/CenterBlock/CenterBlock.jsx
+++ app/components/GameBoard/components/CenterBlock/CenterBlock.jsx
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import './CenterBlock.styl'
 
 class CenterBlock extends Component {
   render () {
     return (
-      <div>
-        <button type='button' onClick={this.props.changeTipPositions}>asdas</button>
+      <div className='game_panel'>
+        <img src='app/components/Type/Images/monopoly-brand.png' className='extended__logo' alt='extended logo' />
+        <fieldset className='buttons_box'>
+          <button className='controls_button' type='button' onClick={this.props.changeTipPositions}>Кинути кубики</button>
+          <button className='controls_button' type='button'>Банк</button>
+          <button className='controls_button' type='button'>Журнал Гри</button>
+        </fieldset>
+        <fieldset className='players_field'>
+          <div className='player'><div className='player_output'>Player1 / 10000$</div><div className='player_btn' >&gt;</div></div>
+          <div className='player'><div className='player_output'>Player1 / 10000$</div><div className='player_btn' >&gt;</div></div>
+          <div className='player'><div className='player_output'>Player1 / 10000$</div><div className='player_btn' >&gt;</div></div>
+          <div className='player'><div className='player_output'>Player1 / 10000$</div><div className='player_btn' >&gt;</div></div>
+        </fieldset>
       </div>
     )
   }
